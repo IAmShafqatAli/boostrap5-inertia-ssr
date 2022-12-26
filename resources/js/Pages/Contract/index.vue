f65f78b2f9b9084ce0a33574e2219332d9576b1e
Shafqat Ali Nawaz <Ali@Faiqas-MBP.fritz.box>
2022-12-26 23:26:40 +0100

    1st commit

---
+++ resources/js/Pages/Contract/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <app-layout title="Contracts">
+<!--        $page.props.jetstream.canUpdateProfileInformation-->
+        <template #header>
+            <h2 class="h4 font-weight-bold">
+              List of All contracts
+            </h2>
+        </template>
+
+    </app-layout>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+export default defineComponent({
+    props: ['contracts'],
+    components: {
+        AppLayout,
+    },
+})
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import('./bootstrap');
+import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
+
+// Import modules...
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    //
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob("./Pages/**/*.vue")),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mixin({ methods: { route } })
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
